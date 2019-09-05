5b7e93fc1707e93a1782bd6e2ab9950640f32cf3
Ashutosh Goyal <axgoyal@gmail.com>
2019-09-04 22:53:06 -0700

    Updated the Operators implementation

--- SimpleOperators.py
+++ SimpleOperators.py
@@ -1,5 +1,24 @@
-## Simple Operators
+## This Program will implement Simple Operators
+
+# Variable declaration
+
+a=15
+b=6
 
 ## Additions:
+c=a+b
+print ("Addition of two values {} and {} is: {} ".format(a, b, c) )
 
-print ()
+## Subtraction:
+c=a-b
+print ("Subtraction of two values {} and {} is: {} ".format(a, b, c) )
+
+
+## Multiplication:
+c=a*b
+print ("Multiplication of two values {} and {} is: {} ".format(a, b, c) )
+
+
+## Division:
+c=a/b
+print ("Division of two values {} and {} is: {} ".format(a, b, c) )
